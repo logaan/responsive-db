1ce66248dfa28b4f2754a88e1769b42f2d287c97
Logan Campbell <colin@logaan.net>
2014-03-04 23:40:18 +1100

    can watch from one repl as you write through another

--- src/responsive_db/core.clj
+++ src/responsive_db/core.clj
@@ -1,6 +1,43 @@
-(ns responsive-db.core)
+(ns responsive-db.core
+  (:require [datomic.api :refer [db q] :as d]
+            [clojure.pprint :refer [pprint]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def schema
+  [{:db/id                  #db/id[:db.part/db]
+    :db/ident               :thing/name
+    :db/valueType           :db.type/string
+    :db/cardinality         :db.cardinality/one
+    :db.install/_attribute  :db.part/db}])
+
+(defn seed [uri]
+  (let [res  (d/create-database uri)
+        conn (d/connect uri)]
+    @(d/transact conn schema)
+    conn))
+
+(defn create [conn thing]
+  (let [id          (d/tempid :db.part/user)
+        transaction @(d/transact conn [(merge {:db/id id} thing)])]
+    (d/resolve-tempid (d/db conn) (:tempids transaction) id)))
+
+(defn id-of-created [transaction]
+  (first (vals (:tempids transaction))))
+
+(comment
+ 
+  (def conn (seed "datomic:free://localhost:4334/responsive-db"))
+
+  (def queue (d/tx-report-queue conn))
+
+  (loop [tx (.take queue)]
+    (let [id (id-of-created tx)
+          e  (d/entity (:db-after tx) id)]
+      (println (:thing/name e))
+      (recur (.take queue))))
+
+  (create conn {:thing/name "Logan"})
+
+  (create conn {:thing/name "Bob"})
+
+  )
+
