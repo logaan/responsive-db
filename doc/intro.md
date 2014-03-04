efe7592bec5526c709d857e01f69bbe01dc71f61
Logan Campbell <colin@logaan.net>
2014-03-04 22:57:41 +1100

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to responsive-db
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/responsive_db/core_test.clj
@@ -0,0 +1,7 @@
+(ns responsive-db.core-test
+  (:require [clojure.test :refer :all]
+            [responsive-db.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
